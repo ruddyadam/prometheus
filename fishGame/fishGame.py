e9a0daf8d4dc1d8706bd69473e90e75eed1108a1
Adam Ekelund <ruddyadam@gmail.com>
2014-01-29 16:01:46 -0600

    added description

--- fishGame/fishGame.py
+++ fishGame/fishGame.py
@@ -1,4 +1,23 @@
 #fishGame.py
+"""
+draw a shark sprite
+    -shark points horizontally towards the closest food drop when dropped
+        -shark location (updates with step)
+        -all food locations (updates with step)
+    -shark's mouth ('center' of sprite is mouth?) moves directly towards food when dropped
+drop food sprite/animation on mouse click
+    -will drop only on single mouse click, not on holding down mouse
+    -will drop with each click
+        -create food sprite instance with mouse click at mouse location  
+
+FUTURE:
+score
+    -number on top shows how many food bits are on the screen.
+    -another number shows how many food bits were eaten
+
+"""
+
+
 
 import pygame, sys
 from pygame.locals import *
@@ -6,8 +25,21 @@ from pygame.locals import *
 pygame.init()
 fpsClock = pygame.time.Clock()
 
+blue = (0, 0, 255)
+shark = pygame.image.load('shark.jpg')
+
+
 screen = pygame.display.set_mode((640, 480))
+bg = screen.fill(blue)
 pygame.display.set_caption('Ultimate Fish Game')
+
+#class sharkSprite(pygame.sprite.Sprite):
+    
+#class foodSprite(pygame.sprite.Sprite):
+
+
+
+
 
 while True: #main game loop
     for event in pygame.event.get():
@@ -15,4 +47,15 @@ while True: #main game loop
             pygame.quit()
             sys.exit()
         pygame.display.update()
+"""
+        if event.type == KEYDOWN:
+            if event.key = K_RIGHT:
+                flip_x = True
+            elif event.key = K_LEFT:
+                flip_x = False
+
+
+        new_image = pygame.transform.flip(image, flip_x, False)
+"""
+        
     
