8d969473383ebe52a8f4ed9deeedee4315844dff
Adam Ekelund <ruddyadam@gmail.com>
2014-01-29 14:44:45 -0600

    black screen shark.png

--- fishGame/fishGame.py
+++ fishGame/fishGame.py
@@ -1,2 +1,18 @@
 #fishGame.py
-#just updating for new key
+
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+fpsClock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Ultimate Fish Game')
+
+while True: #main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        pygame.display.update()
+    
