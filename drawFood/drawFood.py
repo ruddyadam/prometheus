ef03452da43d98b8d9e17b77f368a597ffb24673
Adam Ekelund <ruddyadam@gmail.com>
2014-02-06 17:45:23 -0600

    stamping complete, at least visually.  probably unusable, needs refactoring

--- drawFood/drawFood.py
+++ drawFood/drawFood.py
@@ -13,16 +13,37 @@ color = (255,255,255)
 radius = (10)
 bg_image = 'aquarium.jpg'
 background = pygame.image.load(bg_image).convert()
+rectCentx = 2
+rectCenty = 2
+rectHei = 4
+rectWid = 4
 
 
 while True: #main game loop
+    global x,y
+    x,y = pygame.mouse.get_pos()
+    food = Rect(x-rectCentx,y-rectCenty,rectHei,rectWid)
+
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed() == (True,False,False):
+                #global foodPosList
+                #foodPosTuple = pygame.mouse.get_pos()
+                #for i in foodPosTuple:
+                #    foodPosList.append(i)
+                #need to store coordinates to be retrieved by
+                #this draw.rect and stored so they can be deleted when food gets eaten.  bad idea.,
+                xFood,yFood = pygame.mouse.get_pos()
+                foodStamp = Rect(xFood-rectCentx,yFood-rectCenty,rectHei,rectWid)
+                screen.lock()
+                pygame.draw.rect(background, color, foodStamp, 0)
+                screen.unlock()
+                print xFood-rectCentx,'\'', yFood-rectCenty
 
-    x,y = pygame.mouse.get_pos()
-    food = Rect(x-3,y-3,8,8)
             
     screen.blit(background, (0,0))
     
@@ -31,7 +52,8 @@ while True: #main game loop
     pygame.draw.rect(screen, color, food, 0)
     screen.unlock()
 
-    
-
+    #on mouseclick, stamp an image of the food
+    #store food coordinates in an array, that will be the list we can delete from when they get eaten.
+        
 
     pygame.display.update()
