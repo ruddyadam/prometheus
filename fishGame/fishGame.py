0f5318328571be3139d302d574f93eb762ddfe1c
Adam Ekelund <ruddyadam@gmail.com>
2014-02-14 20:20:46 -0600

    bugfix required: line 156 ValueError.

--- fishGame/fishGame.py
+++ fishGame/fishGame.py
@@ -1,35 +1,5 @@
 #fishGame.py
 
-# place fish with right mouseclick
-# place food with left mouseclick
-
-# loop fish movement:
-# update coordinates of fish / food
-# draw fish
-# draw fish eye
-# draw food
-# clear screen
-
-"""
-draw a shark sprite
-    -shark points horizontally towards the closest food drop when dropped
-        -shark location (updates with step)
-        -all food locations (updates with step)
-    -shark's mouth ('center' of sprite is mouth?) moves directly towards food when dropped
-drop food sprite/animation on mouse click
-    -will drop only on single mouse click, not on holding down mouse
-    -will drop with each click
-        -create food sprite instance with mouse click at mouse location  
-
-FUTURE:
-score
-    -number on top shows how many food bits are on the screen.
-    -another number shows how many food bits were eaten
-
-"""
-
-
-
 import pygame, sys
 from pygame.locals import *
 
@@ -46,10 +16,6 @@ VIOLET = (208, 32, 144)
 WHITE = (255, 255, 255)
 bg_image = 'aquarium.jpg'
 
-food_positions = [] # a list of tuples containing x,y coordinates of 'food'
-fish_positions = [] # a list of tuples containing x,y coordinates of the 'nose' of the fish
-
-
 #todo: make the fish move less fast, but do not reduce FPS, or some clicks will not register.
 #todo: set up a counter in the fish loop, that the fish will only move after each fish's position is re-updated 10 times.
 #todo: this will make each fish 10x slower by design, without reducing the fps (amount of main loops per second)
@@ -60,9 +26,8 @@ def main(): #the main function
     global background
     global screen
     #global food_positions and fish_positions are needed for the 'Escape' key to work.
-    global food_positions
-    global fish_positions
-
+    food_positions = [] # a list of tuples containing x,y coordinates of 'food'
+    fish_positions = [] # a list of tuples containing x,y coordinates of the 'nose' of the fish
     screen = pygame.display.set_mode((screenRect.size),0,32) # this creates the screen
     pygame.display.set_caption('Ultimate Fish Game:             Right Click : FISH  |  Left Click : FOOD  |  Escape : CLEAR') # the title
 
@@ -92,57 +57,79 @@ def main(): #the main function
 
         #screen.fill((0,0,0)) # clears screen/ fills the screen with black
         screen.blit(background, (0,0))      #this keeps redrawing the background
-        draw_fishes() # draw fish based on right click
-        draw_foods() # draw food based on left click
+        draw_fishes(fish_positions) # draw fish based on right click
+        draw_foods(food_positions) # draw food based on left click
         move_fishes_towards_food(fish_positions, food_positions)
         fps_clock.tick(fps)
 
-
-
         # screen.blit(mouse_cursor,(x,y))     #redraws the mouse cursor image at x,y (cursor position)
-        
         pygame.display.update()  #not sure what this does
 
 
-        #if n[0] == len(moveList): # adding to the list
-
-
-def draw_fishes(): # draw every fish, using fishPos locations
-
-    for n in fish_positions: # n[0] = x, n[1] = y coordinates
-
-        fish_body_coordinates_relative_to_nose = [(n[0]-90, n[1]+9), (n[0]-121, n[1]+20), (n[0]-120, n[1]-21), (n[0]-92, n[1]-3), (n[0]-39, n[1]-29), (n[0], n[1]), (n[0]-37, n[1]+24)]
-        fish_eye_centerpoint = (n[0]-33, n[1]-10)
-        fish_eye_radius = 7
-
-        screen.lock()
-        # draws fish body
-        pygame.draw.polygon(screen, BLUE, fish_body_coordinates_relative_to_nose, 0) # draw fish body
-        pygame.draw.circle(screen, YELLOW, fish_eye_centerpoint, fish_eye_radius,0) # draw fish eye
-        screen.unlock()
-
-
-def draw_foods(): # draw every food, suing foodPos locations
-
-    for food_position in food_positions:
-        # the '-2' is so that the cursor is in the center of the square, not at the upper left corner. '5' is length, height
-        food_square = Rect(food_position[0]-2,food_position[1]-2,5,5)
-        screen.lock()
-        pygame.draw.rect(screen, WHITE, food_square, 0)
-        screen.unlock()
-        #print str(n[0]) + ',' + str(n[1])
-
-def move_fishes_towards_food(fish_positions, food_positions):
+def draw_fishes(fish_positions):
     """
-    Moves each fish towards the food.
-
-    @param food_positions
-    a list of 2-lists which represent x, y coordinates of the food
+    draws every fish, using fish_positions locations
 
     @param fish_positions
     a list of 2-lists which represent x,y coordinates of the fishes
     """
 
+    for body_coordinate in fish_positions: # n[0] = x, n[1] = y coordinates
+
+        fish_body_coordinates_relative_to_nose = [
+            (body_coordinate[0]-90,  body_coordinate[1]+9),
+            (body_coordinate[0]-121, body_coordinate[1]+20),
+            (body_coordinate[0]-120, body_coordinate[1]-21),
+            (body_coordinate[0]-92,  body_coordinate[1]-3),
+            (body_coordinate[0]-39,  body_coordinate[1]-29),
+            (body_coordinate[0],     body_coordinate[1]),
+            (body_coordinate[0]-37,  body_coordinate[1]+24)
+        ]
+
+        fish_eye_centerpoint = (body_coordinate[0]-33, body_coordinate[1]-10)
+        fish_eye_radius = 7
+
+        screen.lock()
+        # draws fish body
+        pygame.draw.polygon(screen, BLUE, fish_body_coordinates_relative_to_nose, 0)
+        #draws fish eye
+        pygame.draw.circle(screen, YELLOW, fish_eye_centerpoint, fish_eye_radius,0)
+        screen.unlock()
+
+def draw_foods(food_positions):
+    """
+    draws all food in food_positions
+
+    @param food_positions
+    a list of 2-lists which represent x, y coordinates of the food
+    """
+
+    for food_location in food_positions:
+        # the '-2' is so that the cursor is in the center of the square, not at the upper left corner. '5' is length, height
+        food_square = Rect(food_location[0]-2,food_location[1]-2,5,5)
+        screen.lock()
+        pygame.draw.rect(screen, WHITE, food_square, 0)
+        screen.unlock()
+
+def remove_first_food_from_list(food_positions):
+    food_positions = food_positions[1:]
+    return food_positions
+
+
+
+def move_fishes_towards_food(fish_positions, food_positions):
+    """
+    Moves each fish towards the food.
+
+    @param fish_positions
+    a list of 2-lists which represent x,y coordinates of the fishes
+
+    @param food_positions
+    a list of 2-lists which represent x, y coordinates of the food
+
+    @return
+    a list of 2-lists which represent x, y coordinates of the food
+    """
     # if there are things in food_positions, then there is food on the screen
     if len(food_positions) > 0:
         current_food_position = food_positions[0]
@@ -152,29 +139,25 @@ def move_fishes_towards_food(fish_positions, food_positions):
             # moves the fish if the fish is farther than fish_move_distance pixels from the food
             #print "\nfood_positions line 156: ", food_positions
             #print "food_positions line 158: ", food_positions
-            if len(food_positions) > 0: #todo: added as an attempt to stop the error.
-                if food_x_distance_from_fish > fish_move_distance or food_y_distance_from_fish > fish_move_distance:
-                    # Move the fish towards the food, if it's above the food, move it down,
-                    # If it's below food, move it up
-                    for axis in fish_position:
-                        if axis > current_food_position[fish_position.index(axis)]:
-                            # if fish is below or right, changes the x or y coordinate to get fish_move_distance pixels closer to the food
-                            fish_position[fish_position.index(axis)] = axis - fish_move_distance
-                        else:
-                            # if fish is above or left, changes the x or y coordinate to get fish_move_distance pixels closer to the food
-                            fish_position[fish_position.index(axis)] = axis + fish_move_distance
-                else:
-                    for axis in fish_position:
-                        fish_position[fish_position.index(axis)] = current_food_position[fish_position.index(axis)]
-                    # todo: error, trying to remove a list item that is not there.
-                    # todo: 'ValueError: list.remove(x): x not in list', and exit code 1
-                    # todo: could be if the fish are too close together?
-                    # todo: it happens when I add food while
-                    # todo: it does not happen when there is only 1 pice of food on the screen.
-                    # todo: it happens when the fishes (multiple fish) are locked on to a food, and I add a food.
-                    # todo: also, fish get 'stuck' sometimes until I add another fish.
-                    if len(food_positions) > 0: # todo: added as an attempt to stop the error
-                        food_positions.remove(current_food_position)
+            if food_x_distance_from_fish > fish_move_distance or food_y_distance_from_fish > fish_move_distance:
+                # Move the fish towards the food, if it's above the food, move it down,
+                # If it's below food, move it up
+                for axis in fish_position:
+                    if axis > current_food_position[fish_position.index(axis)]:
+                        # if fish is below or right, changes the x or y coordinate to move closer to the food
+                        fish_position[fish_position.index(axis)] = axis - fish_move_distance
+                    else:
+                        # if fish is above or left, changes the x or y coordinate to move closer to the food
+                        fish_position[fish_position.index(axis)] = axis + fish_move_distance
+            else:
+                for axis in fish_position:
+                    fish_position[fish_position.index(axis)] = current_food_position[fish_position.index(axis)]
+                #todo: error on 156. occurs: 2+ fish, when food is supposed to be removed. crashes when fish nose == food coordinate
+                food_positions.remove(current_food_position)
+
+
+                #food_positions = remove_first_food_from_list(food_positions)
+
                     # makes the closest fish's nose coordinates match the food coordinates ('eating')
                     # removes that food coordinate from the food_positions
                     # this will be for any fish whose position is <= fish_move_distance pixels away.
@@ -184,15 +167,21 @@ def move_fishes_towards_food(fish_positions, food_positions):
 def test():
     """
     this test moves the fish coordinates toward the food one step (10 pixels).
+
+    @param food_positions
+    a list of 2-lists which represent x, y coordinates of the food
+
+    @param fish_positions
+    a list of 2-lists which represent x,y coordinates of the fishes
     """
 
-    fish_positions = [[100,101]]
-    food_positions = [[200,201]]
+    fishes = [[100,101]] # fish_positions
+    foods = [[200,201]]  #food#positions
 
-    move_fishes_towards_food(fish_positions, food_positions)
+    move_fishes_towards_food(fishes, foods)
 
-    print "\nresult: fish_positions = ", fish_positions # [[110,111]]
-    print "result: food_positions = ", food_positions   # [[200,201]]
+    print "\nresult: fish_positions = ", fishes # [[110,111]]
+    print "result: food_positions = ", foods   # [[200,201]]
 
 
 if __name__ == '__main__': #this allows import of this without automatic code execution
@@ -324,3 +313,33 @@ def setFishPos(): # updates the moveList
         # if Nx[n]-xFood[n] < 10 (move distance) pixels, and Ny[n]-yFood[n] == 0, eatFood() # puts a fish on food location
         # possible bug if fish tie.  make it so closest fish (in pixels) wins
         # ... eatFood() iterates
+
+# place fish with right mouseclick
+# place food with left mouseclick
+
+# loop fish movement:
+# update coordinates of fish / food
+# draw fish
+# draw fish eye
+# draw food
+# clear screen
+
+"""
+draw a shark sprite
+    -shark points horizontally towards the closest food drop when dropped
+        -shark location (updates with step)
+        -all food locations (updates with step)
+    -shark's mouth ('center' of sprite is mouth?) moves directly towards food when dropped
+drop food sprite/animation on mouse click
+    -will drop only on single mouse click, not on holding down mouse
+    -will drop with each click
+        -create food sprite instance with mouse click at mouse location
+
+FUTURE:
+score
+    -number on top shows how many food bits are on the screen.
+    -another number shows how many food bits were eaten
+
+"""
+
+
